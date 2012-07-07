14dcacea540fb3d3783d97898498691ea3f9ad07
Sandro Turriate <sandro.turriate@gmail.com>
2012-07-07 14:12:13 -0700

    Init

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# Running the app
+
+    $ gem install sinatra
+    $ ruby app.rb
+
+## Notes
+All movies are loaded and stored in memory. Each page refresh resets state.
---
+++ public/movies.js
@@ -0,0 +1,75 @@
+function Movie(name, openingWeekend, totalGross) {
+  this.name = name;
+  this.openingWeekend = openingWeekend;
+  this.totalGross = totalGross;
+  this.slug = function() {
+    return this.name.toLowerCase().replace(/\s+/g,'-');
+  }
+  this.isNew = typeof(this.name) != 'string'
+}
+
+var avengers = new Movie('The Avengers', 207438708, 538116000);
+var harryPotter = new Movie('Harry Potter and the Deathly Hallows Part 2', 169189427, 381011219);
+
+function moviesController($scope, state) {
+  console.log('moviescontroller');
+  $scope.state = state;
+}
+
+function newMovieController($scope, state, $location) {
+  $scope.state = state;
+  $scope.newMovie = new Movie;
+  $scope.submit = function() {
+    if ($scope.form.$valid) {
+      state['movies'].push($scope.newMovie);
+      $location.path('/');
+    }
+    else {
+      $scope.form.$error['base'] = 'There was an error!'
+    }
+  }
+}
+
+function movieController($scope, state, $routeParams) {
+  $scope.movie = state.findMovieBySlug($routeParams['slug']);
+}
+
+var module = angular.module('movieApp', [], function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+  $routeProvider.when('/', {
+    templateUrl: 'list.html',
+    controller: moviesController
+  });
+  $routeProvider.when('/new', {
+    templateUrl: 'list.html',
+    controller: newMovieController
+  });
+  $routeProvider.when('/:slug', {
+    templateUrl: 'show.html',
+    controller: movieController
+  });
+});
+
+module.directive('fadeIn', function() {
+  return function(scope, element, attrs) {
+    var movie = scope.movie;
+    if (movie.isNew) {
+      $(element).hide().fadeIn('slow', function() { movie.isNew = false});
+    }
+   };
+});
+
+module.service('state', function() {
+  return {
+    'movies': [],
+    'findMovieBySlug': function (slug) {
+        for (movieIndex in this.movies) {
+          var movie = this.movies[movieIndex];
+          if (movie.slug() == slug) return movie;
+        }
+    },
+    'populate': function() {
+      this['movies'] = [avengers, harryPotter];
+    }
+  }
+});
